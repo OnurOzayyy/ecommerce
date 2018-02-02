ee3250583334ad45ec3d34b6b609656d4d39497e
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-02-02 00:20:52 -0800

    Add Crispy Forms

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+
     'shop',
 ]
 
@@ -129,3 +131,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
