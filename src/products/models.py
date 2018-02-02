c94f0a179d85311571a1fc8d0d1a0172ab1deacb
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-02-02 11:46:47 -0800

    Add Model Manager to the Product Model

--- src/products/models.py
+++ src/products/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from django.urls import reverse
 
+
+
+
+
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self, *args, **kwargs):
+        return self.get_queryset().active()
+
+
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField(blank=True, null=True)
     price           = models.DecimalField(decimal_places=2, max_digits=20)
     active          = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
