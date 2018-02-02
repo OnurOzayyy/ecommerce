958ca1d1c72111726e9d102f65fb8478fa0216d9
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-02-02 12:05:48 -0800

    Add Variation Model and Register to Admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from products.models import Product
+from products.models import Product, Variation
 
-admin.site.register(Product)
+
+class VariationInline(admin.TabularInline):
+    model = Variation
+    extra = 0
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'price', 'id']
+    inlines = [
+        VariationInline
+    ]
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
--- src/products/models.py
+++ src/products/models.py
@@ -3,8 +3,6 @@ from django.urls import reverse
 
 
 
-
-
 class ProductQuerySet(models.query.QuerySet):
     def active(self):
         return self.filter(active=True)
@@ -26,8 +24,32 @@ class Product(models.Model):
 
     objects = ProductManager()
 
+    class Meta:
+        ordering = ["-title"]
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('product_detail', kwargs={'pk': self.pk})
+
+
+class Variation(models.Model):
+    product     = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title       = models.CharField(max_length=120)
+    price       = models.DecimalField(decimal_places=2, max_digits=20)
+    active      = models.BooleanField(default=True)
+    inventory   = models.IntegerField(null=True, blank=True) # -1 or none refers to unlimited
+    sale_price  = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_price(self):
+        if self.sale_price is not None:
+            return self.sale_price
+        else:
+            return self.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
