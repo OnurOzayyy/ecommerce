a45d6be5d49d2919e219f2dfa66783e46a8be1e3
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-02-02 11:41:35 -0800

    Add Product List View

--- src/products/urls.py
+++ src/products/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path, re_path, include
 
 
-from .views import ProductDetailView
+from .views import ProductDetailView, ProductListView
 
 urlpatterns = [
     re_path('^(?P<pk>\d+)/$', ProductDetailView.as_view() , name='product_detail'),
+    re_path('^$', ProductListView.as_view(), name='products'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from django.utils import timezone
 
 from .models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        context['now'] =timezone.now()
+        context["query"] = self.request.GET.get('q')
+        return context
 
 
 class ProductDetailView(DetailView):
