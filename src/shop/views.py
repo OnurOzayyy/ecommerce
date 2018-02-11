16c044ccd75d1fe54471a9205fdc21668d9a96cd
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-02-10 22:07:55 -0800

    Add Shopping Cart Icon and Fix Some Styling

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 
+from products.models import Product
+
 
 def home_page(request):
+    products = Product.objects.all()
     context = {
-        "title": 'Welcome to SF Home Design'
+        "title": 'Welcome to SF Home Design',
+        "products": products
     }
     return render(request, 'shop/home.html', context)
