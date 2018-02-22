0fa1ee5cd942d30c4ed56627514b3c9bfd453661
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-02-22 10:42:21 -0800

    Cart models, view and template

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,6 +1,10 @@
+from decimal import Decimal
 from django.db import models
 from django.conf import settings
 from products.models import Variation
+from django.db.models.signals import pre_save, post_save
+
+
 
 class CartItem(models.Model):
     """
@@ -9,7 +13,7 @@ class CartItem(models.Model):
     cart = models.ForeignKey("Cart", on_delete=models.CASCADE)
     item = models.ForeignKey(Variation, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-
+    line_item_total = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.item.title
@@ -22,6 +26,23 @@ class CartItem(models.Model):
         return self.item.remove_from_cart()
 
 
+def cart_item_pre_save_receiver(sender, instance, *args, **kwargs):
+    """
+    Sets the price for item before it is saved to the database.
+    """
+    price = instance.item.get_price()
+    qty = instance.quantity
+    line_item_total =  Decimal(price) * Decimal(qty)
+    instance.line_item_total = line_item_total
+
+pre_save.connect(cart_item_pre_save_receiver, sender=CartItem)
+
+def cart_item_post_save_receiver(sender, instance, *args, **kwargs):
+    instance.cart.update_subtotal()
+
+post_save.connect(cart_item_post_save_receiver, sender=CartItem)
+
+
 class Cart(models.Model):
     """
     Cart Model.
@@ -30,7 +51,23 @@ class Cart(models.Model):
     items = models.ManyToManyField(Variation, through=CartItem)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    subtotal = models.DecimalField(max_digits=6, decimal_places=2, default=25.00)
 
 
     def __str__(self):
         return str(self.id)
+
+    def update_subtotal(self):
+        """
+        Calculate the cart subtotal by adding the each item's price.
+        """
+        print('updating subtotal...')
+        subtotal = 0
+        items = self.cartitem_set.all()
+        print(items)
+        for item in items:
+            subtotal += item.line_item_total
+        self.subtotal = subtotal
+        self.save()
+        print("subtotal :", subtotal)
+
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
-from django.http import HttpResponseRedirect, Http404
-
+from django.http import HttpResponseRedirect, Http404, JsonResponse
+from django.urls import reverse
 
 from products.models import Variation
 from carts.models import CartItem, Cart
@@ -12,7 +12,7 @@ class CartView(View):
     template_name = "carts/view.html"
 
 
-    def get_cart_object(self, *args, **kwargs):
+    def get_object(self, *args, **kwargs):
         """
         Checks if cart exists in the session.
         if not, then creates and saves a new cart object and
@@ -40,25 +40,51 @@ class CartView(View):
         Adds or Deletes the item, obtaining the item id and quantity from
         the Get request.
         """
-        cart = self.get_cart_object()
+        cart = self.get_object()
         item_id = request.GET.get("item")
-        delete_item = request.GET.get("delete")
+        delete_item = request.GET.get("delete", False)
+        item_added = False
         if item_id:
+            print('inside if item id ',item_id)
             item_instance = get_object_or_404(Variation, id=item_id)
-            qty = request.GET.get("qty",1)
+            qty = request.GET.get("qty", 1)
             try:
-                if int(qty) < 1:
+                if int(qty) <= 0:
                     delete_item = True
             except:
                 raise Http404
-            cart_item = CartItem.objects.get_or_create(cart=cart, item=item_instance)[0]
+            cart_item, created = CartItem.objects.get_or_create(cart=cart, item=item_instance)
+
+            if created:
+                item_added = True
             if delete_item:
                 cart_item.delete()
             else:
                 cart_item.quantity = qty
                 cart_item.save()
+            if not request.is_ajax():
+                return HttpResponseRedirect(reverse("cart"))
+
+        if request.is_ajax():
+            try:
+                total = cart_item.line_item_total
+            except:
+                total = None
+            try:
+                subtotal = cart_item.cart.subtotal
+            except:
+                subtotal = None
+            data = {
+                    "deleted": delete_item,
+                    "item_added": item_added,
+                    "subtotal": subtotal,
+                    "line_total": total
+                    }
+            print(request.GET.get("item"))
+            return JsonResponse(data)
+
         context = {
-            "object": self.get_cart_object()
+            "object": self.get_object()
         }
         template = self.template_name
         return render(request, template, context)
